791fac3d2c1db0359a1d54923cd8b9c998d22ed5
mdfaysalbabu <foysalfoysal313@gmail.com>
2024-01-30 08:02:50 +0600

    login and registration

--- src/components/layOut/MainLayout.tsx
+++ src/components/layOut/MainLayout.tsx
@@ -8,7 +8,9 @@ const MainLayout = () => {
       <Sidebar />
       <div className="flex-1 p-3 m-auto md:p-6 lg:p-5 ">
         <h1 className="text-2xl font-bold mb-4 md:mb-8 lg:mb-12">
-          <Outlet />
+          <div>
+            <Outlet />
+          </div>
         </h1>
       </div>
     </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -50,73 +50,81 @@ const Register = () => {
     }
   };
   return (
-    <div className="w-full max-w-md p-8 space-y-3 rounded-xl border b  font-sans mx-auto lg:w-[500px] drop-shadow-lg bg-teal-200 mt-12">
-      <h1 className="text-3xl font-bold text-center text-indigo-600">
-        Register
-      </h1>
+    <div className="container mx-auto">
+      <div className="w-full max-w-md p-10 space-y-3 rounded-xl border b  font-sans mx-auto lg:w-[500px] drop-shadow-lg bg-teal-200 h-screen overflow-hidden  items-center justify-center  ">
+        <div className="md:mt-6 mt-10">
+          <h1 className="text-3xl font-bold text-center text-indigo-600">
+            Register
+          </h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} action="" className="space-y-4 ">
-        <div className="space-y-2 text-sm">
-          <label htmlFor="name" className="block ">
-            Your name
-          </label>
-          <input
-            {...register("username")}
-            type="text"
-            id="username"
-            placeholder="username"
-            className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
-          />
-        </div>
-        <div className="space-y-2 text-sm">
-          <label htmlFor="email" className="block ">
-            Your Email
-          </label>
-          <input
-            {...register("email")}
-            type="text"
-            id="email"
-            placeholder="email"
-            className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
-          />
-        </div>
-        <div className="space-y-2 text-sm">
-          <label htmlFor="password" className="block ">
-            Password
-          </label>
-          <input
-            {...register("password")}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
-          />
-        </div>
-        <button
-          type="submit"
-          className="text-lg rounded-xl relative p-[10px] block w-full bg-indigo-600 text-white border-y-4 duration-500 overflow-hidden focus:border-indigo-500 z-50 group"
-        >
-          Register
-          <span className="absolute opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-1000 ease-out flex justify-center inset-0 items-center z-10 text-white">
-            Let's go
-          </span>
-          <span className="bg-indigo-800 absolute inset-0 -translate-y-full group-hover:translate-y-0 group-hover:duration-1000"></span>
-          <span className="bg-indigo-800 absolute inset-0 translate-y-full group-hover:translate-y-0 group-hover:duration-1000"></span>
-          <span className="bg-indigo-800 absolute inset-0 translate-x-full group-hover:translate-x-0 group-hover:delay-300 delay-100 duration-1000"></span>
-          <span className="bg-indigo-800 absolute inset-0 -translate-x-full group-hover:translate-x-0 group-hover:delay-300 delay-100 duration-1000"></span>
-        </button>
-      </form>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            action=""
+            className="space-y-4 "
+          >
+            <div className="space-y-2 text-sm">
+              <label htmlFor="name" className="block ">
+                Your name
+              </label>
+              <input
+                {...register("username")}
+                type="text"
+                id="username"
+                placeholder="username"
+                className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
+              />
+            </div>
+            <div className="space-y-2 text-sm">
+              <label htmlFor="email" className="block ">
+                Your Email
+              </label>
+              <input
+                {...register("email")}
+                type="text"
+                id="email"
+                placeholder="email"
+                className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
+              />
+            </div>
+            <div className="space-y-2 text-sm">
+              <label htmlFor="password" className="block ">
+                Password
+              </label>
+              <input
+                {...register("password")}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                className="w-full px-4 py-3 rounded-md border border-indigo-300 focus:outline-none focus:ring  "
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-lg rounded-xl relative p-[10px] block w-full bg-indigo-600 text-white border-y-4 duration-500 overflow-hidden focus:border-indigo-500 z-50 group"
+            >
+              Register
+              <span className="absolute opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-1000 ease-out flex justify-center inset-0 items-center z-10 text-white">
+                Let's go
+              </span>
+              <span className="bg-indigo-800 absolute inset-0 -translate-y-full group-hover:translate-y-0 group-hover:duration-1000"></span>
+              <span className="bg-indigo-800 absolute inset-0 translate-y-full group-hover:translate-y-0 group-hover:duration-1000"></span>
+              <span className="bg-indigo-800 absolute inset-0 translate-x-full group-hover:translate-x-0 group-hover:delay-300 delay-100 duration-1000"></span>
+              <span className="bg-indigo-800 absolute inset-0 -translate-x-full group-hover:translate-x-0 group-hover:delay-300 delay-100 duration-1000"></span>
+            </button>
+          </form>
 
-      <p className="text-sm text-center gap-2 flex justify-center sm:px-6 ">
-         have an account
-        <Link
+          <p className="text-sm text-center gap-2 flex justify-center sm:px-6 ">
+            have an account
+            <Link
               to="/login"
               className="font-semibold text-gray-800 hover:underline focus:text-gray-800 focus:outline-none ms-1 text-md"
             >
               Login
             </Link>
-      </p>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
